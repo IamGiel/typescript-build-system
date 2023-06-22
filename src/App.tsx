c52754c7a06771c411cd9a612ae6cc8b0bfa3923
alpha_giel <alpha.giel@beroe-inc.com>
2023-06-22 11:23:34 -0400

    set up declarations.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import './styles.scss';
+import IMAGE_TWO from './assets/images/some_image2.jpeg';
+import IMAGE_STUDY from './assets/images/study.svg';
 import React from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+    <> 
+      <div className='app'>App</div>
+      <img src={IMAGE_TWO} alt="PNG IMAGE" />
+      <img src={IMAGE_STUDY} alt="STUDY IMAGE" />
+    </>
   )
 }
