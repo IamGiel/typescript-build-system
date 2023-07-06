0af203952448d259d2c9bf480c7666d1d1f60d4f
alpha.giel@beroe-inc.com <alpha.giel@beroe-inc.com>
2023-07-06 08:56:30 -0400

    push build

---
+++ build/dest/shared/index.ts
@@ -0,0 +1,10 @@
+import { PepComponentLoader, PepComponent, PepProfile } from './pep';
+import { LiveTable, IPEProfile, ITableProps } from './live-table';
+export {
+  PepComponent,
+  PepComponentLoader,
+  PepProfile,
+  LiveTable,
+  IPEProfile,
+  ITableProps,
+};
